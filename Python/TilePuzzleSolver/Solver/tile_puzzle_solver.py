b6e19e66f0bbeb7ff063ce073e8b49349dc1b3d9
Sudharsan <sudharsansci@gmail.com>
2020-05-24 08:25:42 -0400

    Feat: Add check solution feasibility of inputs using isSolvable()
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- Python/TilePuzzleSolver/Solver/tile_puzzle_solver.py
+++ Python/TilePuzzleSolver/Solver/tile_puzzle_solver.py
@@ -82,6 +82,47 @@ class TilePuzzleSolver:
         self.que.put(self.current_node)
 
     @staticmethod
+    def isSolvable(initial_state: list, goal_state: list) -> bool:
+        """
+        isSolvable(initial_state, goal_state) -> bool
+
+        It takes the initial state and goal state unique number and checks for solution feasibility.
+
+        Args:
+            initial_state: int
+                A 9 digit integer
+            goal_state: int
+                A 9 digit integer
+
+        Returns: bool
+            Returns True if the solution is feasible for the given states else False.
+
+        """
+
+        # ---> Step 01: Copy the arg <--- #
+        init, goal = initial_state.copy(), goal_state.copy()
+
+        # ---> Step 02: Remove zero from the args <--- #
+        init.remove(0)
+        goal.remove(0)
+
+        # ---> Step 03: Initialize flip counter <---#
+        init_flip_count, goal_flip_count = 0, 0
+
+        # initialize Miscellaneous variable
+        n = len(init)
+
+        for i in range(0, n):
+            temp_i = init[i:]
+            temp_g = goal[i:]
+            for j in range(0, len(temp_i)):
+                if temp_i[j] < init[i]:
+                    init_flip_count += 1
+                if temp_g[j] < goal[i]:
+                    goal_flip_count += 1
+        return init_flip_count % 2 == 0 and goal_flip_count % 2 == 0
+
+    @staticmethod
     def generateUniqueNumber(node: np.array, unique_seq: np.array = np.array([100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1])) -> int:
         """
         Args:
